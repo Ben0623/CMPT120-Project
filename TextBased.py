3b1ceb9ce8a8afd3395d99d71030b0f48511f91a
Ben0623 <benjamin.volino1@marist.edu>
2020-01-27 21:17:53 -0500

    Save 2

--- TextBased.py
+++ TextBased.py
@@ -2,6 +2,25 @@
 #Creator: Ben Volino
 #Date: 1/27/20
 def main():
-    print ("you are going to make a PB and J sandwich. to win the \n game you must make the sandwich correctly")
+    Name = input("Enter a name to use")
+    print ("you,", Name, "are going to make a PB and J sandwich. to win the \n game you must make the sandwich correctly")
+    input ("press enter to continue")
+    print ("you are feeling hungry and enter the kitchen to make something to eat you \n need to gather things to make the sandwich")
+    input ("press enter to enter the bread stash")
+    print ("you,", Name,"enter the bread stash and take out 2 peices of bread and put it on the counter")
+    input ("press Enter to go get the PB from the shelf")
+    print ("you obtain the PB from the top shelf after reaching for 10 mins. you then put the PB next to the bread")
+    input ("Press Enter to go get the Jelly from the fridge")
+    print ("you go into the fridge and notice it wasnt wasnt plugged in.\n You,", Name," say screw it and decide rotten jelly will need to do \n you put the jelly on the counter next to the PB")
+    input ("now press enter to get a butter knife to make the sandwich")
+    print ("you go to where ur knives are but you only have spoons so u take one")
+    input ("Press ENTER to make the sandwich")
+    input ("Now press Enter to take a bite")
+    print ("The rotten Jelly promptly makes you projectile vomit everywhere")
+    input ("Press enter to TERMINATE this game")
+    quit 
+        
+    
 main()
+
 
